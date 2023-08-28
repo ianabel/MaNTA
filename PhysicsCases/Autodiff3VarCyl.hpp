a0c640e14cd35823cf0c9d25511d60f20e467058
Edward Tocco <eatocco@gmail.com>
2023-08-28 19:10:28 -0400

    implement autodiff routines, fluxes for 3 var

---
+++ PhysicsCases/Autodiff3VarCyl.hpp
@@ -0,0 +1,70 @@
+#ifndef AUTODIFF3VarCyl_HPP
+#define AUTODIFF3VarCyl_HPP
+
+#include "PhysicsCases.hpp"
+#include <autodiff/forward/dual.hpp>
+#include <autodiff/forward/dual/eigen.hpp>
+
+using autodiff::dual;
+using autodiff::dual2nd;
+using autodiff::VectorXdual;
+
+typedef std::function<dual(VectorXdual u, VectorXdual q, dual x, double t)> sigmaFn;
+typedef std::vector<sigmaFn> sigmaFnArray;
+
+typedef std::function<dual(VectorXdual u, VectorXdual q, VectorXdual sigma, dual x, double t)> SourceFn;
+typedef std::vector<SourceFn> SourceFnArray;
+
+typedef std::function<dual2nd(dual2nd x, dual2nd t)> Solution;
+using TestSolVec = std::vector<Solution>;
+
+class Autodiff3VarCyl : public TransportSystem
+{
+public:
+    explicit Autodiff3VarCyl(toml::value const &config);
+    //  Function for passing boundary conditions to the solver
+    Value LowerBoundary(Index i, Time t) const override;
+    Value UpperBoundary(Index i, Time t) const override;
+
+    bool isLowerBoundaryDirichlet(Index i) const override;
+    bool isUpperBoundaryDirichlet(Index i) const override;
+
+    // The same for the flux and source functions -- the vectors have length nVars
+    Value SigmaFn(Index i, const Values &u, const Values &q, Position x, Time t) override;
+    Value Sources(Index i, const Values &u, const Values &q, const Values &sigma, Position x, Time t) override;
+
+    // We need derivatives of the flux functions
+    void dSigmaFn_du(Index i, Values &, const Values &u, const Values &q, Position x, Time t) override;
+    void dSigmaFn_dq(Index i, Values &, const Values &u, const Values &q, Position x, Time t) override;
+
+    // and for the sources
+    void dSources_du(Index i, Values &, const Values &u, const Values &q, Position x, Time t) override;
+    void dSources_dq(Index i, Values &, const Values &u, const Values &q, Position x, Time t) override;
+    void dSources_dsigma(Index i, Values &, const Values &u, const Values &q, Position x, Time t) override;
+
+    // and initial conditions for u & q
+    Value InitialValue(Index i, Position x) const override;
+    Value InitialDerivative(Index i, Position x) const override;
+    double TestSource(Index i, Position x, Time t);
+
+private:
+    Values uL;
+    bool isUpperDirichlet;
+
+    Values uR;
+    bool isLowerDirichlet;
+
+    Position xL;
+    Position xR;
+
+    static sigmaFnArray sigmaVec;
+    static SourceFnArray SourceVec;
+
+    bool isTestProblem;
+
+    static dual2nd TestDirichlet(dual2nd x, dual2nd t, double u_R, double u_L, double x_L, double x_R);
+    static TestSolVec TestSols;
+
+    REGISTER_PHYSICS_HEADER(Autodiff3VarCyl)
+};
+#endif
--- PhysicsCases/MatrixDiffusion.cpp
+++ PhysicsCases/MatrixDiffusion.cpp
@@ -6,108 +6,103 @@
  */
 
 // Needed to register the class
-REGISTER_PHYSICS_IMPL( MatrixDiffusion );
+REGISTER_PHYSICS_IMPL(MatrixDiffusion);
 
-MatrixDiffusion::MatrixDiffusion( toml::value const& config )
+MatrixDiffusion::MatrixDiffusion(toml::value const &config)
 {
 
 	// Construst your problem from user-specified config
 	// throw an exception if you can't. NEVER leave a part-constructed object around
 	// here we need the actual value of the diffusion coefficient, and the shape of the initial gaussian
 
-	if ( config.count( "DiffusionProblem" ) != 1 )
-		throw std::invalid_argument( "There should be a [DiffusionProblem] section if you are using the MatrixDiffusion physics model." );
+	if (config.count("DiffusionProblem") != 1)
+		throw std::invalid_argument("There should be a [DiffusionProblem] section if you are using the MatrixDiffusion physics model.");
 
-	auto const& DiffConfig = config.at( "DiffusionProblem" );
+	auto const &DiffConfig = config.at("DiffusionProblem");
 
-	nVars =         toml::find_or( DiffConfig, "nVars", 1.0 );
-	InitialWidth  = toml::find_or( DiffConfig, "InitialWidth", 0.2 );
-	Centre =        toml::find_or( DiffConfig, "Centre", 0.0 );
+	nVars = toml::find_or(DiffConfig, "nVars", 1.0);
+	InitialWidth = toml::find_or(DiffConfig, "InitialWidth", 0.2);
+	Centre = toml::find_or(DiffConfig, "Centre", 0.0);
 
-	std::vector<double> InitialHeight_v = toml::find< std::vector<double> >( DiffConfig, "InitialHeights" );
+	std::vector<double> InitialHeight_v = toml::find<std::vector<double>>(DiffConfig, "InitialHeights");
 
-	if ( InitialHeight_v.size() != nVars )
-		throw std::invalid_argument( "Initial height vector must have 'nVars' elements" );
+	if (InitialHeight_v.size() != static_cast<long unsigned int>(nVars))
+		throw std::invalid_argument("Initial height vector must have 'nVars' elements");
 
-	InitialHeights.resize( nVars );
-	for ( Index i=0; i < nVars; ++i )
-		InitialHeights[ i ] = InitialHeight_v[ i ];
+	InitialHeights.resize(nVars);
+	for (Index i = 0; i < nVars; ++i)
+		InitialHeights[i] = InitialHeight_v[i];
 
-	Kappa = Matrix::Identity( nVars, nVars );
-
+	Kappa = Matrix::Identity(nVars, nVars);
 }
 
 // Dirichlet Boundary Conditon
-Value MatrixDiffusion::LowerBoundary( Index, Time ) const
+Value MatrixDiffusion::LowerBoundary(Index, Time) const
 {
 	return 0.0;
 }
 
-Value MatrixDiffusion::UpperBoundary( Index, Time ) const
+Value MatrixDiffusion::UpperBoundary(Index, Time) const
 {
 	return 0.0;
 }
 
-bool MatrixDiffusion::isLowerBoundaryDirichlet( Index ) const { return true; };
-bool MatrixDiffusion::isUpperBoundaryDirichlet( Index ) const { return true; };
+bool MatrixDiffusion::isLowerBoundaryDirichlet(Index) const { return true; };
+bool MatrixDiffusion::isUpperBoundaryDirichlet(Index) const { return true; };
 
-
-Value MatrixDiffusion::SigmaFn( Index i, const Values &, const Values & q, Position, Time )
+Value MatrixDiffusion::SigmaFn(Index i, const Values &, const Values &q, Position, Time)
 {
 	double sigma = 0;
-	
-	for ( Index j=0; j < nVars; j++ )
-		sigma += Kappa( i, j )*q[ j ];
+
+	for (Index j = 0; j < nVars; j++)
+		sigma += Kappa(i, j) * q[j];
 
 	return sigma;
 }
 
-Value MatrixDiffusion::Sources( Index, const Values &, const Values &, const Values &, Position, Time )
+Value MatrixDiffusion::Sources(Index, const Values &, const Values &, const Values &, Position, Time)
 {
 	return 0.0;
 }
 
-void MatrixDiffusion::dSigmaFn_dq( Index i, Values& v, const Values&, const Values&, Position, Time )
+void MatrixDiffusion::dSigmaFn_dq(Index i, Values &v, const Values &, const Values &, Position, Time)
 {
-	for ( Index j = 0; j < nVars; ++j )
-		v[ j ] = Kappa( i, j );
+	for (Index j = 0; j < nVars; ++j)
+		v[j] = Kappa(i, j);
 };
 
-void MatrixDiffusion::dSigmaFn_du( Index, Values& v, const Values&, const Values&, Position, Time )
+void MatrixDiffusion::dSigmaFn_du(Index, Values &v, const Values &, const Values &, Position, Time)
 {
-	v = Vector::Zero( nVars );
+	v = Vector::Zero(nVars);
 };
 
-void MatrixDiffusion::dSources_du( Index, Values&v , const Values &, const Values &, Position, Time )
+void MatrixDiffusion::dSources_du(Index, Values &v, const Values &, const Values &, Position, Time)
 {
-	v = Vector::Zero( nVars );
+	v = Vector::Zero(nVars);
 };
 
-void MatrixDiffusion::dSources_dq( Index, Values&v , const Values &, const Values &, Position, Time )
+void MatrixDiffusion::dSources_dq(Index, Values &v, const Values &, const Values &, Position, Time)
 {
-	v = Vector::Zero( nVars );
+	v = Vector::Zero(nVars);
 };
 
-void MatrixDiffusion::dSources_dsigma( Index, Values&v , const Values &, const Values &, Position, Time )
+void MatrixDiffusion::dSources_dsigma(Index, Values &v, const Values &, const Values &, Position, Time)
 {
-	v = Vector::Zero( nVars );
+	v = Vector::Zero(nVars);
 };
-
-
 
 // We don't need the index variables as nVars is 1, so the index argument should
 // always be 0
 
 // Initialise with a Gaussian at x = 0
-Value MatrixDiffusion::InitialValue( Index i, Position x ) const
+Value MatrixDiffusion::InitialValue(Index i, Position x) const
 {
-	double y = ( x - Centre )/InitialWidth;
-	return InitialHeights[ i ] * ::exp( -y*y );
+	double y = (x - Centre) / InitialWidth;
+	return InitialHeights[i] * ::exp(-y * y);
 }
 
-Value MatrixDiffusion::InitialDerivative( Index i, Position x ) const
+Value MatrixDiffusion::InitialDerivative(Index i, Position x) const
 {
-	double y = ( x - Centre )/InitialWidth;
-	return InitialHeights[ i ] * ( -2.0 * y ) * ::exp( -y*y ) * ( 1.0/InitialWidth );
+	double y = (x - Centre) / InitialWidth;
+	return InitialHeights[i] * (-2.0 * y) * ::exp(-y * y) * (1.0 / InitialWidth);
 }
-
