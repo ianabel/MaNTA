aeac12a2a93426f55174ede642a1749dfb3dc383
Edward Tocco <eatocco@gmail.com>
2023-09-13 15:22:29 -0400

    add angular momentum, move stuff to constants

--- PhysicsCases/FourVarCylFlux.cpp
+++ PhysicsCases/FourVarCylFlux.cpp
@@ -1,8 +1,8 @@
-#include "3VarCylFlux.hpp"
+#include "FourVarCylFlux.hpp"
 #include "Constants.hpp"
 #include <iostream>
 
-REGISTER_FLUX_IMPL(ThreeVarCylFlux);
+REGISTER_FLUX_IMPL(FourVarCylFlux);
 
 enum
 {
@@ -10,14 +10,14 @@ enum
     Gaussian = 1,
 };
 
-ThreeVarCylFlux::ThreeVarCylFlux(toml::value const &config, Index nVars)
+FourVarCylFlux::FourVarCylFlux(toml::value const &config, Index nVars)
 {
 
     nVars = nVars;
-    if (config.count("3VarCylFlux") != 1)
-        throw std::invalid_argument("There should be a [3VarCylFlux] section if you are using the 3VarCylFlux physics model.");
+    if (config.count("4VarCylFlux") != 1)
+        throw std::invalid_argument("There should be a [4VarCylFlux] section if you are using the 4VarCylFlux physics model.");
 
-    auto const &DiffConfig = config.at("3VarCylFlux");
+    auto const &DiffConfig = config.at("4VarCylFlux");
 
     std::string profile = toml::find_or(DiffConfig, "SourceType", "None");
     ParticleSource = ParticleSources[profile];
@@ -27,15 +27,16 @@ ThreeVarCylFlux::ThreeVarCylFlux(toml::value const &config, Index nVars)
     sigma.insert(std::pair<Index, sigmaptr>(0, &Gamma_hat));
     sigma.insert(std::pair<Index, sigmaptr>(1, &qe_hat));
     sigma.insert(std::pair<Index, sigmaptr>(2, &qi_hat));
+    sigma.insert(std::pair<Index, sigmaptr>(3, &hi_hat));
 
     source.insert(std::pair<Index, sourceptr>(0, &Sn_hat));
     source.insert(std::pair<Index, sourceptr>(1, &Spe_hat));
     source.insert(std::pair<Index, sourceptr>(2, &Spi_hat));
+    source.insert(std::pair<Index, sourceptr>(3, &Shi_hat));
 };
 
-int ThreeVarCylFlux::ParticleSource;
-double ThreeVarCylFlux::sourceStrength;
-
+int FourVarCylFlux::ParticleSource;
+double FourVarCylFlux::sourceStrength;
 const dual n0 = 3e20;
 const dual T0 = e_charge * 10e3;
 const dual p0 = n0 * T0;
@@ -44,7 +45,7 @@ const dual Gamma0 = p0 / (electronMass * Om_e * Om_e * tau_e(n0, p0));
 const dual V0 = Gamma0 / n0;
 const Value L = 1;
 
-dual ThreeVarCylFlux::Gamma_hat(VectorXdual u, VectorXdual q, dual x, double t)
+dual FourVarCylFlux::Gamma_hat(VectorXdual u, VectorXdual q, dual x, double t)
 {
     // maybe add a factor of sqrt x if x = r^2/2
 
@@ -56,7 +57,7 @@ dual ThreeVarCylFlux::Gamma_hat(VectorXdual u, VectorXdual q, dual x, double t)
     else
         return -G;
 };
-dual ThreeVarCylFlux::qi_hat(VectorXdual u, VectorXdual q, dual x, double t)
+dual FourVarCylFlux::qi_hat(VectorXdual u, VectorXdual q, dual x, double t)
 {
     dual G = Gamma_hat(u, q, x, t);
     dual qri = ::pow(ionMass / electronMass, 1. / 2.) * 1.0 / (::sqrt(2) * tau_e(u(0), u(2))) * 2. * u(2) * u(2) / u(0) * (q(2) / u(2) - q(0) / u(0));
@@ -69,8 +70,22 @@ dual ThreeVarCylFlux::qi_hat(VectorXdual u, VectorXdual q, dual x, double t)
     else
 
         return Q;
+}
+dual FourVarCylFlux::hi_hat(VectorXdual u, VectorXdual q, dual x, double t)
+{
+    dual G = Gamma_hat(u, q, x, t);
+    dual ghi = ::pow(ionMass / electronMass, 1. / 2.) * 1.0 / (::sqrt(2) * tau_e(u(0), u(2))) * 3. / 10. * u(3) * u(2) / u(1) * (q(3) / u(3) - q(0) / u(0));
+    dual H = u(3) * G / u(0) + sqrt(2. * x) * ghi;
+    if (H != H)
+    {
+        return 0.0;
+    }
+    else
+    {
+        return H;
+    }
 };
-dual ThreeVarCylFlux::qe_hat(VectorXdual u, VectorXdual q, dual x, double t)
+dual FourVarCylFlux::qe_hat(VectorXdual u, VectorXdual q, dual x, double t)
 {
     dual G = Gamma_hat(u, q, x, t);
     dual qre = 1.0 / tau_e(u(0), u(1)) * (4.66 * u(1) * u(1) / u(0) * (q(1) / u(1) - q(0) / u(0)) - (3. / 2.) * u(1) / u(0) * (q(2) + q(1)));
@@ -86,7 +101,7 @@ dual ThreeVarCylFlux::qe_hat(VectorXdual u, VectorXdual q, dual x, double t)
         return Q;
 };
 
-dual ThreeVarCylFlux::Sn_hat(VectorXdual u, VectorXdual q, VectorXdual sigma, dual x, double t)
+dual FourVarCylFlux::Sn_hat(VectorXdual u, VectorXdual q, VectorXdual sigma, dual x, double t)
 {
     dual S = 0;
     dual Center = 0.5;
@@ -104,14 +119,21 @@ dual ThreeVarCylFlux::Sn_hat(VectorXdual u, VectorXdual q, VectorXdual sigma, du
 };
 
 // look at ion and electron sources again -- they should be opposite
-dual ThreeVarCylFlux::Spi_hat(VectorXdual u, VectorXdual q, VectorXdual sigma, dual x, double t)
+dual FourVarCylFlux::Spi_hat(VectorXdual u, VectorXdual q, VectorXdual sigma, dual x, double t)
 {
     dual G = -Gamma_hat(u, q, x, t) / (2. * x);
     dual V = G / u(0); //* L / (p0);
     dual S = 2. / 3. * sqrt(2. * x) * V * q(2) + 2. / 3. * Ci(u(0), u(2), u(1));
     return S;
+}
+dual FourVarCylFlux::Shi_hat(VectorXdual u, VectorXdual q, VectorXdual sigma, dual x, double t)
+{
+    dual G = -Gamma_hat(u, q, x, t) / (2. * x);
+    dual V = G / u(0);
+    dual S = 1. / sqrt(2. * x) * (V * u(3) - 1);
+    return S;
 };
-dual ThreeVarCylFlux::Spe_hat(VectorXdual u, VectorXdual q, VectorXdual sigma, dual x, double t)
+dual FourVarCylFlux::Spe_hat(VectorXdual u, VectorXdual q, VectorXdual sigma, dual x, double t)
 {
     dual G = -Gamma_hat(u, q, x, t) / (2. * x);
     dual V = G / u(0); //* L / (p0);
--- plot_nc.py
+++ plot_nc.py
@@ -6,14 +6,23 @@ import matplotlib.pyplot as plt
 
 def main():
 
-    data = xr.open_dataset("./Config/AutodiffTransportSystem.nc")
+    data = xr.open_dataset("./Config/3VarCyl.nc")
     x = data["x"]
     t = data["t"]
+    length = t.shape[0]
 
     plt.figure()
     #plt.plot(x,data["Var0"][0,:])
     plt.contourf(x,t,data["Var0"])
     plt.show()
+    
+    plt.figure()
+    for i in range( int(length/10)):
+    
+        plt.plot(x,data["Var0"][4*i,:])
+
+    plt.show()
+
     data.close()
 
 if __name__ == "__main__":
