7a6c9af4030b9cda1cfb0b1255ac45bf3a49fc77
Ian Abel <ian@utumno.org.uk>
2023-11-07 11:52:45 -0500

    pythonitis

--- python/PythonLinearDiffusion.py
+++ python/PythonLinearDiffusion.py
@@ -1,6 +1,7 @@
 import MaNTA
+import numpy as np
 
-class PythonLinearDiffusion(TransportSystem):
+class PythonLinearDiffusion(MaNTA.TransportSystem):
     def __init__(self, config):
         # Config should be a dict
         self.nVars = 1
@@ -8,22 +9,24 @@ class PythonLinearDiffusion(TransportSystem):
             print("For the stuff transport model, you must specify chi0, kappa, and gamma")
             sys.exit(1)
 
-        self.chi0 = config["chi0"]
+
+        self.Centre = 0.0
+        self.InitialWidth = 0.2
+        self.InitialHeight = 1.0
         self.kappa = config["kappa"]
-        self.gamma = config["gamma"]
-        selc.tprim_crit = config["CriticalGradient"]
-        self.EdgeValue = config["EdgeTemperature"]
+        self.t0 = InitialWidth * InitialWidth / ( 4.0 * kappa );
+        print("t0 = ",t0)
 
 # This problem uses VN lower boundary and dirichlet upper boundary
 # assumed to be on [0,1] in normalised flux
-    def LowerBoundary(self, index, time):
-        return 0.0
+    def LowerBoundary(self, index, t):
+        return self.ExactSolution( 0.0, t )
     def isLowerBoundaryDirichlet(self, index):
-        return False
-    def UpperBoundary(self, index, time):
-        return self.EdgeValue
+        return True
+    def UpperBoundary(self, index, t):
+        return self.ExactSolution( 1.0, t )
     def isUpperBoundaryDirichlet(self, index):
-        return True;
+        return True
 
     def SigmaFn( self, index, uVals, qVals, x, t ):
         tprim = qVals[0]
@@ -37,19 +40,27 @@ class PythonLinearDiffusion(TransportSystem):
     def dSigmaFn_du( self, index, out, u, q, x, t):
         out[0] = 0.0
         
-    def dSources_du( index, out, q, u, x, t ):
+    def dSources_du( self, index, out, q, u, x, t ):
         out[0] = 0.0
 
-    def dSources_dq( index, out, q, u, x, t ):
+    def dSources_dq( self, index, out, q, u, x, t ):
         out[0] = 0.0
 
-    def dSources_dsigma( index, out, q, u, x, t ):
+    def dSources_dsigma( self, index, out, q, u, x, t ):
         out[0] = 0.0
 
-    def InitialValue( index, x ):
+    def InitialValue( self, index, x ):
+        y = ( x - self.Centre )/self.InitialWidth
+        return self.InitialHeight * np.exp( -y*y )
 
-    def InitialDerivative( index, x ):
+    def InitialDerivative( self, index, x ):
+        y = ( x - self.Centre )/self.InitialWidth
+        return self.InitialHeight * ( -2.0 * y ) * np.exp( -y*y ) * ( 1.0/self.InitialWidth )
+
+    def ExactSolution( self, x, t ):
+        EtaSquared = ( x - Centre )*( x - Centre )/( 4.0 * kappa * ( t + t0 ) );
+        return self.InitialHeight * np.sqrt( self.t0/( t + self.t0 ) ) * np.exp( -EtaSquared );
 
 def registerTransportSystems():
-    MaNTA.registerPhysicsCase( "PythonLinearDiffusion", PythonLinearDiffusion.init )
+    MaNTA.registerPhysicsCase( "PythonLinearDiffusion", PythonLinearDiffusion.__init__ )
 
