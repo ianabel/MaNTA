23ddc5652172978e308420e235afdd332d05c16c
Ian Abel <ian@utumno.org.uk>
2023-11-08 10:59:59 -0500

    split logical functions up

--- Tools/vtkConverter/vtkConverter.py
+++ Tools/vtkConverter/vtkConverter.py
@@ -4,3 +4,11 @@
 # Usage ./vtkConverter.py <MaNTA netcdf file> <optional equilibrium file>
 
 # Load plasma data into a 2D array of data then call writeVTK
+
+# Load MaNTA file into 1D arrays, interpolate as functions of Psi
+
+# Load Eq File into 2D array
+
+# Loop over every point
+
+
