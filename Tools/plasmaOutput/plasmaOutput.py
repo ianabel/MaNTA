7071b10a8a87283d484005755e64d2bb60ae34c8
Ian Abel <iga@physics.org>
2023-11-08 16:38:36 -0500

    push

--- Tools/plasmaOutput/plasmaOutput.py
+++ Tools/plasmaOutput/plasmaOutput.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python3
 
-# Simple python script that takes MaNTA outputs and an equilbirium file and, if necessary generates phi_0 at all 
+# Simple python script that takes MaNTA outputs and an equilibrium file and, if necessary, generates phi_0 at all 
 # points before writing a combined netCDF file
 
 from netCDF4 import Dataset
 
-
+class Plasma:
+    def __init__( self, ncFileManta, ncFileEq ):
+        self.MantaData = Dataset(ncFileManta, "r", format="NETCDF4")
+        self.EqData = Dataset(ncFileEq, "r", format="NETCDF4")
