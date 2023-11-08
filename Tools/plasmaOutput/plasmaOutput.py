2885cefbdd10e3d0e39b258e167bb97ab222568b
Ian Abel <ian@utumno.org.uk>
2023-11-08 10:59:20 -0500

    split logical functions up

---
+++ Tools/plasmaOutput/plasmaOutput.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+# Simple python script that takes MaNTA outputs and an equilbirium file and, if necessary generates phi_0 at all 
+# points before writing a combined netCDF file
+
+from netCDF4 import Dataset
+
+
