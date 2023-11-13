45d711cb3a13e438657e6301581c0dfdcdad0586
Ian Abel <iga@physics.org>
2023-11-13 14:00:06 -0500

    sync

--- Tools/vtkConverter/vtkConverter.py
+++ Tools/vtkConverter/vtkConverter.py
@@ -1,14 +1,50 @@
-#!/usr/bin/env python3]
+#!/usr/bin/env python3
 #
 
-# Usage ./vtkConverter.py <MaNTA netcdf file> <optional equilibrium file>
+import numpy as np
+import sys
 
-# Load plasma data into a 2D array of data then call writeVTK
+# Usage ./vtkConverter.py <MaNTA netcdf file> <Equilibrium File> <Output VTK file>
 
-# Load MaNTA file into 1D arrays, interpolate as functions of Psi
+if len(sys.argv) == 4:
+    MantaFile = sys.argv[1]
+    EqFile    = sys.argv[2]
+    outFile   = sys.argv[3]
+else:
+    print("Usage ./vtkConverter.py <MaNTA netcdf file> <Equilibrium File> <Output VTK file>")
+    sys.exit(1)
 
-# Load Eq File into 2D array
 
-# Loop over every point
+import plasmaOutput
+
+# Plamsa.N and Plasma.T are functions taking (species index, psi)
+# Plasma.Psi is a 2D array
+# Plasma.R / Plasma.Z are 1D arrays 
+
+Plasma = plasmaOutput.PlasmaClass( MantaFile, EqFile )
+
+DensityData = np.zeros( (Plasma.R.size,Plasma.Z.size,Plasma.nSpecies) )
+TemperatureData = np.zeros( (Plasma.R.size,Plasma.Z.size,Plasma.nSpecies) )
+OmegaData = np.zeros( (Plasma.R.size,Plasma.Z.size) )
+
+for i in range(Plasma.R.size):
+	for j in range(Plasma.Z.size):
+		psiVal = Plasma.Psi[i,j]
+		phi0 = Plasma.calcPhi0(i,j)
+		omega = Plasma.omega(psiVal)
+		OmegaData[i,j] = omega
+		for s in range(Plasma.nSpecies):
+			T = Plasma.T(s,psiVal)
+			TemperatureData[i,j,s] = T
+			DensityData[i,j,s] = Plasma.N(s,psi) * np.exp( Plasma.m[s] * omega**2 * Plasma.R[i]**2 / (2.0 * T) - Plasma.Z[s] * phi0 / T )
+
+data = [ ("Psi",OmegaData) ]
+
+for s in range(Plasma.nSpecies):
+    data.append( ("n"+Plasma.specie[s],DensityData[:,:,s] ) )
+    data.append( ("T"+Plasma.specie[s],TemperatureData[:,:,s] ) )
 
 
+import vtkOutput
+
+vtkOutput.writeVTK(
