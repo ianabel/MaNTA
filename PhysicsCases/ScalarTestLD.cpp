7923e084f75cd9a16096baed8504fc663c171d7d
Ian Abel <ian@utumno.org.uk>
2023-12-11 20:12:24 -0500

    test for scalar

---
+++ PhysicsCases/ScalarTestLD.cpp
@@ -0,0 +1,116 @@
+
+#include "ScalarTestLD.hpp"
+
+/*
+	Implementation of the Linear Diffusion case
+ */
+
+// Needed to register the class
+REGISTER_PHYSICS_IMPL(ScalarTestLD);
+
+ScalarTestLD::ScalarTestLD(toml::value const &config)
+{
+	// Always set nVars in a derived constructor
+	nVars = 1;
+	nScalars = 1;
+
+	// Construst your problem from user-specified config
+	// throw an exception if you can't. NEVER leave a part-constructed object around
+	// here we need the actual value of the diffusion coefficient, and the shape of the initial gaussian
+
+	if (config.count("DiffusionProblem") != 1)
+		throw std::invalid_argument("There should be a [DiffusionProblem] section if you are using the ScalarTestLD physics model.");
+
+	auto const &DiffConfig = config.at("DiffusionProblem");
+
+	kappa = toml::find_or(DiffConfig, "Kappa", 1.0);
+	InitialWidth = toml::find_or(DiffConfig, "InitialWidth", 0.2);
+	InitialHeight = toml::find_or(DiffConfig, "InitialHeight", 1.0);
+	Centre = toml::find_or(DiffConfig, "Centre", 0.5);
+
+	lowerNeumann = toml::find_or(DiffConfig, "LowerNeumann", false);
+}
+
+// Dirichlet Boundary Conditon
+Value ScalarTestLD::LowerBoundary(Index, Time) const
+{
+	return 0.0;
+}
+
+Value ScalarTestLD::UpperBoundary(Index, Time) const
+{
+	return 0.0;
+}
+
+bool ScalarTestLD::isLowerBoundaryDirichlet(Index) const { return !lowerNeumann; };
+bool ScalarTestLD::isUpperBoundaryDirichlet(Index) const { return true; };
+
+Value ScalarTestLD::SigmaFn(Index, const State &s, Position x, Time)
+{
+	return kappa * s.Derivative[0];
+}
+
+Value ScalarTestLD::Sources(Index, const State &, Position, Time)
+{
+	return 0.0;
+}
+
+void ScalarTestLD::dSigmaFn_dq(Index, Values &v, const State &, Position, Time)
+{
+	v[0] = kappa;
+};
+
+void ScalarTestLD::dSigmaFn_du(Index, Values &v, const State &, Position, Time)
+{
+	v[0] = 0.0;
+};
+
+void ScalarTestLD::dSources_du(Index, Values &v, const State &, Position, Time)
+{
+	v[0] = 0.0;
+};
+
+void ScalarTestLD::dSources_dq(Index, Values &v, const State &, Position, Time)
+{
+	v[0] = 0.0;
+};
+
+void ScalarTestLD::dSources_dsigma(Index, Values &v, const State &, Position, Time)
+{
+	v[0] = 0.0;
+};
+
+// We don't need the index variables as nVars is 1, so the index argument should
+// always be 0
+
+// Initialise with a Gaussian at x = 0
+Value ScalarTestLD::InitialValue(Index, Position x) const
+{
+	double y = (x - Centre) / InitialWidth;
+	return InitialHeight * ::exp(-y * y);
+}
+
+Value ScalarTestLD::InitialDerivative(Index, Position x) const
+{
+	double y = (x - Centre) / InitialWidth;
+	return -InitialHeight * (-2.0 * y) * ::exp(-y * y) * (1.0 / InitialWidth);
+}
+
+Value ScalarTestLD::ScalarG( Index, const DGSoln & y, Time )
+{
+	return y.Scalar( 0 );
+}
+
+void ScalarTestLD::ScalarGPrime( Index, State &s, const DGSoln &y, std::function<double( double )>, Interval, Time )
+{
+	s.Flux[ 0 ] = 0.0;
+	s.Derivative[ 0 ] = 0.0;
+	s.Variable[ 0 ] = 0.0;
+	s.Scalars[ 0 ] = 1.0;
+}
+
+void ScalarTestLD::dSources_dScalars( Index, Values &v, const State &, Position, Time )
+{
+	v[ 0 ] = 0.0;
+}
+
---
+++ PhysicsCases/ScalarTestLD.hpp
@@ -0,0 +1,56 @@
+#ifndef SCALARTESTLD_HPP
+#define SCALARTESTLD_HPP
+
+#include "PhysicsCases.hpp"
+
+/*
+	Linear Diffusion Test Case with a trivial scalar
+ */
+
+// Always inherit from TransportSystem
+class ScalarTestLD : public TransportSystem {
+	public:
+		// Must provide a constructor that constructs from a toml configuration snippet
+		// you can ignore it, or read problem-dependent parameters from the configuration file
+		explicit ScalarTestLD( toml::value const& config );
+
+		// You must provide implementations of both, these are your boundary condition functions
+		Value LowerBoundary( Index, Time ) const override;
+		Value UpperBoundary( Index, Time ) const override;
+
+		bool isLowerBoundaryDirichlet( Index ) const override;
+		bool isUpperBoundaryDirichlet( Index ) const override;
+
+		// The guts of the physics problem (these are non-const as they
+		// are allowed to alter internal state such as to store computations
+		// for future calls)
+		Value SigmaFn( Index, const State &, Position, Time ) override;
+		Value Sources( Index, const State &, Position, Time ) override;
+
+		void dSigmaFn_du( Index, Values &, const State &, Position, Time ) override;
+		void dSigmaFn_dq( Index, Values &, const State &, Position, Time ) override;
+
+		void dSources_du( Index, Values&v , const State &, Position, Time ) override;
+		void dSources_dq( Index, Values&v , const State &, Position, Time ) override;
+		void dSources_dsigma( Index, Values&v , const State &, Position, Time ) override;
+
+
+		virtual Value ScalarG( Index, const DGSoln& , Time ) override;
+		virtual void ScalarGPrime( Index, State &, const DGSoln &, std::function<double( double )>, Interval, Time ) override;
+		virtual void dSources_dScalars( Index, Values &, const State &, Position, Time ) override;
+
+		// Finally one has to provide initial conditions for u & q
+		Value      InitialValue( Index, Position ) const override;
+		Value InitialDerivative( Index, Position ) const override;
+
+private:
+	// Put class-specific data here
+	double kappa, InitialWidth, InitialHeight, Centre;
+	bool lowerNeumann;
+
+	// Without this (and the implementation line in ScalarTestLD.cpp)
+	// ManTA won't know how to relate the string 'ScalarTestLD' to the class.
+	REGISTER_PHYSICS_HEADER( ScalarTestLD )
+};
+
+#endif // SCALARTESTLD_HPP
