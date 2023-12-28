3c8f2aa4a801257c9a919dd71b93fe7df6c2f566
Ian Abel <iabel@umd.edu>
2023-12-28 06:11:26 -0500

    couple more testing cases

---
+++ PhysicsCases/TwoChannelNonlinear.hpp
@@ -0,0 +1,46 @@
+#ifndef ADJOINTPOSTER_HPP
+#define ADJOINTPOSTER_HPP
+
+#include "PhysicsCases.hpp"
+
+class TwoChannelNonlinear : public TransportSystem
+{
+public:
+	// Must provide a constructor that constructs from a toml configuration snippet
+	// you can ignore it, or read problem-dependent parameters from the configuration file
+	explicit TwoChannelNonlinear(toml::value const &config, Grid const & );
+
+	// You must provide implementations of both, these are your boundary condition functions
+	Value LowerBoundary(Index, Time) const override;
+	Value UpperBoundary(Index, Time) const override;
+
+	bool isLowerBoundaryDirichlet(Index) const override;
+	bool isUpperBoundaryDirichlet(Index) const override;
+
+	// The guts of the physics problem (these are non-const as they
+	// are allowed to alter internal state such as to store computations
+	// for future calls)
+	Value SigmaFn(Index, const State &, Position, Time) override;
+	Value Sources(Index, const State &, Position, Time) override;
+
+	void dSigmaFn_du(Index, Values &, const State &, Position, Time) override;
+	void dSigmaFn_dq(Index, Values &, const State &, Position, Time) override;
+
+	void dSources_du(Index, Values &v, const State &, Position, Time) override;
+	void dSources_dq(Index, Values &v, const State &, Position, Time) override;
+	void dSources_dsigma(Index, Values &v, const State &, Position, Time) override;
+
+	// Finally one has to provide initial conditions for u & q
+	Value InitialValue(Index, Position) const override;
+	Value InitialDerivative(Index, Position) const override;
+
+private:
+	// Put class-specific data here
+	double a, SourceCentre,SourceWidth,T_s;
+
+	// Without this (and the implementation line in TwoChannelNonlinear.cpp)
+	// ManTA won't know how to relate the string 'TwoChannelNonlinear' to the class.
+	REGISTER_PHYSICS_HEADER(TwoChannelNonlinear)
+};
+
+#endif // ADJOINTPOSTER_HPP
