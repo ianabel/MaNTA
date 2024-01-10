9665bf9697bf1e2dd24303018cc8c13dea58c698
Ian Abel <ian@utumno.org.uk>
2024-01-09 22:33:57 -0500

    Cylindrical Plasma for debugging

--- PhysicsCases/AutodiffTransportSystem.hpp
+++ PhysicsCases/AutodiffTransportSystem.hpp
@@ -28,21 +28,22 @@ public:
     virtual Value InitialValue(Index i, Position x) const override;
     virtual Value InitialDerivative(Index i, Position x) const override;
 
+protected:
+	Position xR,xL;
 private:
 	 // API to underlying flux model
-	 virtual Real Flux( Index, RealVector, RealVector, Position, Time ) = 0;
-	 virtual Real Source( Index, RealVector, RealVector, RealVector, Position, Time ) = 0;
+	virtual Real Flux( Index, RealVector, RealVector, Position, Time ) = 0;
+	virtual Real Source( Index, RealVector, RealVector, RealVector, Position, Time ) = 0;
 
-	 static std::vector<int> InitialProfile;
-    std::map<std::string, int> InitialProfiles = {{"Gaussian", 0}, {"Dirichlet", 1}, {"Cosine", 2}, {"Uniform", 3}, {"Linear", 4}};
+	static std::vector<int> InitialProfile;
+	std::map<std::string, int> InitialProfiles = {{"Gaussian", 0}, {"Dirichlet", 1}, {"Cosine", 2}, {"Uniform", 3}, {"Linear", 4}};
 
-    static Vector InitialHeights;
+	static Vector InitialHeights;
 
-	 Position xR,xL;
 
-    static autodiff::dual2nd DirichletIC(Index i, autodiff::dual2nd x, autodiff::dual2nd t, double u_R, double u_L, double x_L, double x_R);
+	static autodiff::dual2nd DirichletIC(Index i, autodiff::dual2nd x, autodiff::dual2nd t, double u_R, double u_L, double x_L, double x_R);
 
-    static autodiff::dual2nd InitialFunction(Index i, autodiff::dual2nd x, autodiff::dual2nd t, double u_R, double u_L, double x_L, double x_R);
+	static autodiff::dual2nd InitialFunction(Index i, autodiff::dual2nd x, autodiff::dual2nd t, double u_R, double u_L, double x_L, double x_R);
 
 };
 #endif
